2a50c3c1281ac62d2f6f38ac35589a1b845e4d92
REMR11 <ronaldmejia0211@gmail.com>
2025-08-13 15:05:04 -0600

    fix: Corrige error de imports en repositories

--- src/main/java/org/kodigo/swagger_librery/swagger/repository/LibroRepository.java
+++ src/main/java/org/kodigo/swagger_librery/swagger/repository/LibroRepository.java
@@ -1,6 +1,6 @@
 package org.kodigo.swagger_librery.swagger.repository;
 
-import com.biblioteca.api.entity.Libro;
+import org.kodigo.swagger_librery.swagger.entity.Libro;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
